2aab810e9b9a84f9d899f7a0824e7fd18a7f6f60
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2024-09-06 04:43:58 -0400

    Hice el modelo

---
+++ RegistroTecnicos/RegistroTecnicos/TecnicosRegistro/TecnicosRegistro/Models/Tecnicos.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TecnicosRegistro.Models;
+
+public class Tecnicos
+{
+	[Key]
+	public int TecnicoId { get; set; }
+
+	[RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "No se permiten caracteres especiales")]
+	[Required(ErrorMessage = "Nombre obligatorio")]
+	public string? Nombres { get; set; }
+
+	[Range(0.01, float.MaxValue, ErrorMessage = "Ingrese un valor mayor a 0")]
+	[Required(ErrorMessage = "Sueldo obligtorio")]
+	public decimal SueldoHora { get; set; }
+
+}
