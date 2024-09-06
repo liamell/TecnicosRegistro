26b26d7df44c32a8011fd129c514c20dee7aef63
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2024-09-06 04:53:19 -0400

    Hice el service

---
+++ RegistroTecnicos/RegistroTecnicos/TecnicosRegistro/TecnicosRegistro/Service/TecnicoService.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using TecnicosRegistro.DAL;
+using TecnicosRegistro.Models;
+
+namespace TecnicosRegistro.Service;
+
+public class TecnicoService
+{
+	private readonly Contexto _contexto;
+
+	public TecnicoService(Contexto contexto)
+	{
+		_contexto = contexto;
+	}
+
+	public async Task<bool> Guardar(Tecnicos tecnico)
+	{
+		if (!await Existe(tecnico.TecnicoId))
+			return await Insertar(tecnico);
+		else
+			return await Modificar(tecnico);
+	}
+
+	private async Task<bool> Existe(int tecnicoId)
+	{
+		return await _contexto.Tecnicos
+			.AnyAsync(t => t.TecnicoId == tecnicoId);
+	}
+
+	public async Task<bool> ExisteTecnico(int tecnicoId, string nombre)
+	{
+		return await _contexto.Tecnicos
+			.AnyAsync(t => t.TecnicoId != tecnicoId
+			&& t.Nombres.ToLower().Equals(nombre.ToLower()));
+	}
+
+
+
+	private async Task<bool> Insertar(Tecnicos tecnico)
+	{
+		_contexto.Tecnicos.Add(tecnico);
+		return await _contexto.SaveChangesAsync() > 0;
+	}
+
+	private async Task<bool> Modificar(Tecnicos tecnico)
+	{
+		_contexto.Update(tecnico);
+		var modificado = await _contexto.SaveChangesAsync() > 0;
+		_contexto.Entry(tecnico).State = EntityState.Detached;
+		return modificado;
+	}
+	public async Task<bool> Eliminar(Tecnicos tecnico)
+	{
+		return await _contexto.Tecnicos
+			.AsNoTracking()
+			.Where(t => t.TecnicoId == tecnico.TecnicoId)
+			.ExecuteDeleteAsync() > 0;
+	}
+
+	public async Task<Tecnicos?> BuscarId(int id)
+	{
+		return await _contexto.Tecnicos
+			.AsNoTracking()
+			.FirstOrDefaultAsync(t => t.TecnicoId == id);
+	}
+
+	public async Task<List<Tecnicos>> Listar(Expression<Func<Tecnicos, bool>> criterio)
+	{
+		return await _contexto.Tecnicos
+			.AsNoTracking()
+			.Where(criterio)
+			.ToListAsync();
+
+	}
+}
