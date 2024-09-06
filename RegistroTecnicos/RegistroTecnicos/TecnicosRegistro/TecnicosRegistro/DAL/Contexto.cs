46baa00df710053a00103fc692f91a87fa98fbe0
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2024-09-06 04:46:08 -0400

    Hice el contexto

---
+++ RegistroTecnicos/RegistroTecnicos/TecnicosRegistro/TecnicosRegistro/DAL/Contexto.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TecnicosRegistro.Models;
+
+namespace TecnicosRegistro.DAL;
+
+public class Contexto : DbContext
+{
+	public Contexto(DbContextOptions<Contexto> options) : base(options)
+	{
+
+	}
+
+	public DbSet<Tecnicos> Tecnicos { get; set; }
+
+
+}
+
