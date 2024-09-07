b2ebc132040634308835e17c4a298d9e7b42748d
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2024-09-07 18:49:41 -0400

    Hice el modelo de tipo tecnico

---
+++ RegistroTecnicos/RegistroTecnicos/TecnicosRegistro/TecnicosRegistro/Models/TipoTecnico.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TecnicosRegistro.Models;
+
+public class TipoTecnico
+{
+    [Key]
+    public int TipoId { get; set; }
+
+	[RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "No se aceptan números ni caracteres especiales")]
+	[Required(ErrorMessage = "Descripción Obligatoria")]
+	public string? Descripcion { get; set; }
+
+}
