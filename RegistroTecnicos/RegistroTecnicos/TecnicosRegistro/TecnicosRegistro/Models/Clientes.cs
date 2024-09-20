89d92e061ff3cf136b008ef17de085fa3a6558c3
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2024-09-20 15:51:41 -0400

    Modelo cliente

---
+++ RegistroTecnicos/RegistroTecnicos/TecnicosRegistro/TecnicosRegistro/Models/Clientes.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TecnicosRegistro.Models;
+
+public class Clientes
+{
+    [Key]
+    public int ClienteId { get; set; }
+
+	[RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "No se permiten caracteres especiales")]
+	[Required(ErrorMessage = "Nombre obligatorio")]
+	public string? Nombres { get; set; }
+
+	[Required(ErrorMessage = "WhatsApp obligatorio")]
+	[RegularExpression(@"^\d{7,15}$", ErrorMessage = "El número de WhatsApp debe contener entre 7 y 15 dígitos.")]
+	public string? WhatssApp { get; set; }
+}
+
+
+
+
