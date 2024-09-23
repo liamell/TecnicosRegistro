4eda6d67c3795563b0529af66dd3756715ba6fe8
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2024-09-23 00:12:36 -0400

    Modelo

---
+++ RegistroTecnicos/RegistroTecnicos/TecnicosRegistro/TecnicosRegistro/Models/Prioridad.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TecnicosRegistro.Models
+{
+    public class Prioridad
+    {
+        [Key]
+        public int PrioridadId { get; set; }
+
+        [Required(ErrorMessage = "La descripción es obligatoria.")]
+        [StringLength(100, ErrorMessage = "La descripción no puede superar los 100 caracteres.")]
+        public string? Descripcion { get; set; }
+
+        [Required(ErrorMessage = "El tiempo es obligatorio.")]
+        [Range(1, int.MaxValue, ErrorMessage = "El tiempo debe ser un valor positivo.")]
+        public int Tiempo { get; set; }
+    }
+}
