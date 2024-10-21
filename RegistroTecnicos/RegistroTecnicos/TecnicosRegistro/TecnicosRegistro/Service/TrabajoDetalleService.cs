7dd8be534bd378e7c344ace32f4a00c5ce54464a
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2024-10-21 00:51:01 -0400

    TrabajoDetalle service

---
+++ RegistroTecnicos/RegistroTecnicos/TecnicosRegistro/TecnicosRegistro/Service/TrabajoDetalleService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using TecnicosRegistro.DAL;
+using TecnicosRegistro.Models;
+
+namespace TecnicosRegistro.Service;
+
+public class TrabajoDetalleService(Contexto contexto)
+{
+	public async Task<List<Articulo>> Listar(Expression<Func<Articulo, bool>> criterio)
+	{
+		return await contexto
+			.Articulo
+			.Where(criterio)
+			.ToListAsync();
+	}
+}
