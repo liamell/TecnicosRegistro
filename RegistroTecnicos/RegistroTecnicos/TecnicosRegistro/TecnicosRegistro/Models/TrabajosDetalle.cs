3ad6041afaaeafa5a8ee6748c98367602f3ce067
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2024-10-21 00:49:03 -0400

    TrabajoDetalle model

---
+++ RegistroTecnicos/RegistroTecnicos/TecnicosRegistro/TecnicosRegistro/Models/TrabajosDetalle.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TecnicosRegistro.Models;
+
+public class TrabajosDetalle
+{
+	[Key]
+
+	public int DetalleId { get; set; }
+
+	[ForeignKey("TrabajoId")]
+	public int TrabajoId { get; set; }
+	public Trabajo? Trabajo { get; set; }
+
+	[ForeignKey("ArticuloId")]
+	public int ArticuloId { get; set; }
+	public Articulo? Articulo { get; set; }
+
+	[Required(ErrorMessage = "Campo obligatorio")]
+	public int Cantidad { get; set; }
+
+	[Required(ErrorMessage = "Campo obligatorio")]
+	public double Precio { get; set; }
+
+	[Required(ErrorMessage = "Campo obligatorio")]
+	public double Costo { get; set; }
+
+}
