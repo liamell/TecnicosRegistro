fb7c037ce6dbeb8481c1bab4f7aba9e49113c6c3
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2024-10-28 18:18:40 -0400

    Creacion de cotizacion service

---
+++ RegistroTecnicos/RegistroTecnicos/TecnicosRegistro/TecnicosRegistro/Service/CotizacionDetalleService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using TecnicosRegistro.DAL;
+using TecnicosRegistro.Models;
+
+namespace TecnicosRegistro.Service;
+
+public class CotizacionDetalleService(IDbContextFactory<Contexto> DbFactory)
+{
+
+    public async Task<Articulo> Buscar(int id)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Articulo.AsNoTracking().FirstOrDefaultAsync(p => p.ArticuloId == id);
+    }
+    public async Task<List<Articulo>> Listar(Expression<Func<Articulo, bool>> criterio)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Articulo.Where(criterio).ToListAsync();
+    }
+}
---
+++ RegistroTecnicos/RegistroTecnicos/TecnicosRegistro/TecnicosRegistro/Service/CotizacionService.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using TecnicosRegistro.DAL;
+using TecnicosRegistro.Models;
+
+namespace TecnicosRegistro.Service;
+
+public class CotizacionService(IDbContextFactory<Contexto> DbFactory)
+{
+        private async Task<bool> Existe(int cotizacionId)
+        {
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            return await contexto.Cotizar.AnyAsync(c => c.CotizacionId == cotizacionId);
+        }
+
+        private async Task<bool> Insertar(Cotizar cotizar)
+        {
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            contexto.Cotizar.Add(cotizar);
+                return await contexto.SaveChangesAsync() > 0;
+        }
+
+        private async Task<bool> Modificar(Cotizar cotizar)
+        {
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            contexto.Cotizar.Update(cotizar);
+            var modificado = await contexto.SaveChangesAsync() > 0;
+                return modificado;
+        }
+
+        public async Task<bool> Guardar(Cotizar cotizar)
+        {
+            if (!await Existe(cotizar.CotizacionId))
+                return await Insertar(cotizar);
+            else
+                return await Modificar(cotizar);
+        }
+
+        public async Task<bool> Eliminar(int cotizacionId)
+        {
+
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            return await contexto.Cotizar
+            .Include(d => d.cotizacionesDetalle)
+            .Where(c => c.CotizacionId == cotizacionId)
+             .ExecuteDeleteAsync() > 0;
+        }
+
+        public async Task<Cotizar> Buscar(int id)
+        {
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            return await contexto.Cotizar
+            .Include(d => d.cotizacionesDetalle)
+            .FirstOrDefaultAsync(c => c.CotizacionId == id);
+        }
+        public async Task<Cotizar> BuscarConDetalles(int Id)
+        {
+
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            return await contexto.Cotizar
+            .Include(t => t.Cliente)
+             .Include(t => t.cotizacionesDetalle)
+             .ThenInclude(td => td.Articulo)
+             .FirstOrDefaultAsync(t => t.CotizacionId == Id);
+        }
+        public async Task<List<Cotizar>> Listar(Expression<Func<Cotizar, bool>> criterio)
+        {
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            return await contexto.Cotizar
+                .Include(c => c.Cliente)
+                .Include(d => d.cotizacionesDetalle)
+                .AsNoTracking()
+                .Where(criterio)
+                .ToListAsync();
+        }
+}
