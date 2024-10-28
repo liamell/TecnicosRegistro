41147ea6695e32a60d4a60b053a488b8d4723d53
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2024-10-28 18:11:37 -0400

    Hice el modelo cotizar

---
+++ RegistroTecnicos/RegistroTecnicos/TecnicosRegistro/TecnicosRegistro/Models/Cotizar.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TecnicosRegistro.Models;
+
+public class Cotizar
+{
+    [Key]
+    public int CotizacionId { get; set; }
+
+    [Required(ErrorMessage = "Campo obligatorio")]
+    public DateTime Fecha { get; set; } = DateTime.Now;
+
+    [ForeignKey("ClienteId")]
+    public int ClienteId { get; set; }
+    public Clientes? Cliente { get; set; }
+
+    [Required(ErrorMessage = "Campo obligatorio")]
+    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Solo se permiten letras")]
+    public string? Observacion { get; set; }
+
+    [Required(ErrorMessage = "Campo obligatorio")]
+    public decimal Monto { get; set; }
+
+    [ForeignKey("CotizacionId")]
+    public ICollection<CotizarDetalles> cotizacionesDetalle { get; set; } = new List<CotizarDetalles>();
+    
+}
