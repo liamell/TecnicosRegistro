81bf71cdb8564951931d3bd7241dae02fb1ab205
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2024-10-28 18:12:10 -0400

    Hice Modelo cotizar detalles

---
+++ RegistroTecnicos/RegistroTecnicos/TecnicosRegistro/TecnicosRegistro/Models/CotizarDetalles.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TecnicosRegistro.Models;
+
+public class CotizarDetalles
+{
+
+    [Key]
+    public int DetalleId { get; set; }
+
+    [ForeignKey("Cotizar")]
+    public int CotizacionId { get; set; }
+    public Cotizar? Cotizar { get; set; }
+
+    [ForeignKey("Articulo")]
+    public int ArticuloId { get; set; }
+    public Articulo? Articulo { get; set; }
+
+    [Required(ErrorMessage = "Campo obligatorio")]
+    public int Cantidad { get; set; }
+
+    [Required(ErrorMessage = "Campo obligatorio")]
+    public decimal Precio { get; set; }
+}
